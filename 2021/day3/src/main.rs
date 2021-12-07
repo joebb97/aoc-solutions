45844fba0c4d2d42488b8454c28b2c1ed88a6f01
Joseph Buiteweg <joebb@umich.edu>
2021-12-07 10:11:46 -0500

    Continue day 3

--- 2021/day3/src/main.rs
+++ 2021/day3/src/main.rs
@@ -1,22 +1,29 @@
 use std::io::{self, BufRead};
+use bitvec::prelude::*;
 
 fn main() -> Result<(), ()>{
-    let (gamma, epsilon): (u32, u32) = io::stdin().lock().lines()
-    .reduce(|(depth, horiz), line| {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    let mut num_lines = 0;
+    let mut len: Option<usize> = None;
+    let counts: Option<Vec<u32>> = lines
+    .fold(None, |counts,  line| {
         let line = line.unwrap();
-        let (instr, amt): (&str, i32) = (line[0], line[1].parse().unwrap());
-        let new_depth = match instr {
-            "up" => depth - amt,
-            "down" => depth + amt,
-            _ => depth
-        };
-        let new_horiz = match instr {
-            "forward" => horiz + amt,
-            _ => horiz
-        };
-        (new_depth, new_horiz)
+        num_lines += 1;
+        if let None = len {
+            len = Some(line.len());
+        }
+        line.chars().enumerate().fold(counts, |mut counts, (idx, c)| {
+            if let Some(ref mut counts) = counts {
+                counts[idx] += c.to_digit(10).unwrap();
+            } else {
+                counts = Some(vec![0; len.unwrap()]);
+            }
+            counts
+        })
     });
-    println!("ans = gamma * epsilon = {} * {} = {}", 
-             gamma, epsilon, gamma * epsilon);
+    println!("{:?} {:?} {}", counts, len, num_lines);
+    // println!("ans = gamma * epsilon = {} * {} = {}", 
+    //          gamma, epsilon, gamma * epsilon);
     Ok(())
 }
