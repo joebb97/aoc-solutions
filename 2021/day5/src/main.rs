24c68c88ba232af4c110687d8ed2614488b2c545
Joseph Buiteweg <joebb@umich.edu>
2022-01-02 00:46:23 -0500

    Finish day5 both parts

--- 2021/day5/src/main.rs
+++ 2021/day5/src/main.rs
@@ -1,142 +1,230 @@
+use itertools::Itertools;
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::io::{self, BufRead, StdinLock};
+use std::ops::{Mul, Sub};
 
-#[derive(Debug)]
-struct Tile {
-    row: u32,
-    col: u32,
-    called: bool,
-}
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+struct Coord(i32, i32);
 
-type Board = HashMap<u8, Tile>;
-// map from bingo value to tile for quick lookups
-type Boards = Vec<Board>;
+type VentLine = (Coord, Coord);
+type VentLines = Vec<VentLine>;
 
-fn get_boards(lines: std::io::Lines<StdinLock<'_>>) -> Boards {
-    let mut board_length: Option<(usize, usize)> = None;
-    #[derive(Debug)]
-    struct FoldState {
-        cur_board: Board,
-        all_boards: Boards,
-        cur_row: u32,
+impl Sub for Coord {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0, self.1 - other.1)
     }
-    let starting_state = FoldState {
-        cur_board: Board::new(),
-        all_boards: Boards::new(),
-        cur_row: 0,
-    };
-    let boards: Boards = lines
-        .fold(starting_state, |mut state, line| {
-            if let Ok(ref line) = line {
-                if line == "" {
-                    return state;
-                }
-                line.split_whitespace()
-                    .enumerate()
-                    .map(|(y, c)| {
-                        let datum = c.parse().unwrap();
-                        (datum, y)
-                    })
-                    .for_each(|(datum, y)| {
-                        let new_tile = Tile {
-                            row: state.cur_row,
-                            col: y as u32,
-                            called: false,
-                        };
-                        state.cur_board.insert(datum, new_tile);
-                    });
-                state.cur_row += 1;
-                if let None = board_length {
-                    let len = state.cur_board.len();
-                    board_length = Some((len, len * len));
-                }
-                if state.cur_board.len() == board_length.unwrap().1 {
-                    let mut new_state = FoldState {
-                        cur_row: 0,
-                        cur_board: Board::new(),
-                        all_boards: state.all_boards,
-                    };
-                    new_state.all_boards.push(state.cur_board);
-                    new_state
-                } else {
-                    state
-                }
-            } else {
-                state
-            }
-        })
-        .all_boards;
-    return boards;
 }
 
-fn apply_move<'a>(num_called: &'a u8, boards: &'a mut Boards) -> Option<&'a Board> {
-    boards
-        .iter_mut()
+impl Mul for Coord {
+    type Output = i32;
+    fn mul(self, rhs: Self) -> Self::Output {
+        (self.0 * rhs.1) - (rhs.0 * self.1)
+    }
+}
+
+fn direction(pi: Coord, pj: Coord, pk: Coord) -> i32 {
+    (pk - pi) * (pj - pi)
+}
+
+fn on_segment(Coord(xi, yi): Coord, Coord(xj, yj): Coord, Coord(xk, yk): Coord) -> bool {
+    let min_xi_xj = min(xi, xj);
+    let max_xi_xj = max(xi, xj);
+    let min_yi_yj = min(yi, yj);
+    let max_yi_yj = max(yi, yj);
+    (min_xi_xj <= xk && xk <= max_xi_xj) && (min_yi_yj <= yk && yk <= max_yi_yj)
+}
+
+fn segments_intersect(p1: Coord, p2: Coord, p3: Coord, p4: Coord) -> bool {
+    let d1 = direction(p3, p4, p1);
+    let d2 = direction(p3, p4, p2);
+    let d3 = direction(p1, p2, p3);
+    let d4 = direction(p1, p2, p4);
+    if ((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) && ((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0)) {
+        true
+    } else if d1 == 0 && on_segment(p3, p4, p1) {
+        true
+    } else if d2 == 0 && on_segment(p3, p4, p2) {
+        true
+    } else if d3 == 0 && on_segment(p1, p2, p3) {
+        true
+    } else if d4 == 0 && on_segment(p1, p2, p4) {
+        true
+    } else {
+        false
+    }
+}
+
+fn all_points_on_line(l: &VentLine) -> Vec<Coord> {
+    let l_type = get_line_type(&l);
+    let (p1, p2) = l;
+    let (Coord(x1, y1), Coord(x2, y2)) = (p1, p2);
+    let y_max = max(y1, y2);
+    let y_min = min(y1, y2);
+    let x_max = max(x1, x2);
+    let x_min = min(x1, x2);
+    match l_type {
+        LineType::Vert => {
+            let l_diff = y_max - y_min;
+            (0..l_diff + 1)
+                .map(|offset| Coord(p1.0, y_min + offset))
+                .collect()
+        }
+        LineType::Horiz => {
+            let l_diff = x_max - x_min;
+            (0..l_diff + 1)
+                .map(|offset| Coord(x_min + offset, p1.1))
+                .collect()
+        }
+        LineType::Diag => {
+            let steps = x_max - x_min;
+            let x_dir = if x2 > x1 {1} else {-1};
+            let y_dir = if y2 > y1 {1} else {-1};
+            let slope = 1;
+            let ret = (0..steps + 1)
+                .map(|step| {
+                    let x_offset = slope * step * x_dir;
+                    let y_offset = slope * step * y_dir;
+                    Coord(x1 + x_offset, y1 + y_offset)
+                })
+                .collect();
+            // println!("Diag line {:?} points are {:?}", l, ret);
+            ret
+        }
+        LineType::Unknown => {
+            vec![*p1, *p2]
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct PointInfo(u32, bool);
+
+fn update_counts_impl(
+    mut cur_counts: HashMap<Coord, PointInfo>,
+    (coord, point_info): (&Coord, &PointInfo),
+) -> HashMap<Coord, PointInfo> {
+    match cur_counts.get_mut(coord) {
+        Some(&mut existing_point_info) => {
+            let new_point_info = match (point_info, existing_point_info) {
+                (PointInfo(new_marks, false), PointInfo(old_marks, true)) => {
+                    PointInfo(old_marks + new_marks, true)
+                }
+                (PointInfo(new_marks, true), PointInfo(old_marks, false)) => {
+                    PointInfo(new_marks + old_marks, true)
+                },
+                (_, PointInfo(_, true)) => existing_point_info,
+                _ => existing_point_info,
+            };
+            cur_counts.insert(*coord, new_point_info);
+        }
+        None => {
+            cur_counts.insert(*coord, *point_info);
+        }
+    }
+    cur_counts
+}
+
+fn num_intersections(lines: VentLines) -> usize {
+    let counts: HashMap<Coord, PointInfo> = HashMap::new();
+    lines
+        .iter()
         .enumerate()
-        .fold(None, |winning_board, (_, board)| {
-            if winning_board.is_some() {
-                return winning_board;
+        .flat_map(|(idx, coord)| std::iter::repeat(coord).zip(lines.iter().skip(idx + 1)))
+        .fold(counts, |counts, (&l1, &l2)| {
+            let (p1, p2) = l1;
+            let (p3, p4) = l2;
+            if segments_intersect(p1, p2, p3, p4) {
+                let point_intersections: HashMap<Coord, PointInfo> = HashMap::new();
+                let point_intersections = all_points_on_line(&l1)
+                    .iter()
+                    .chain(all_points_on_line(&l2).iter())
+                    .fold(point_intersections, |mut cur_dict, point| {
+                        let entry = cur_dict.entry(*point).or_insert(PointInfo(0, false));
+                        let PointInfo(marks, is_cross) = entry;
+                        *marks += 1;
+                        if *marks >= 2 {
+                            *is_cross = true
+                        }
+                        cur_dict
+                    });
+                return point_intersections.iter().fold(counts, update_counts_impl);
             }
-            if let Some(ent) = board.get_mut(num_called) {
-                ent.called = true;
-            }
-            if let Some(ent) = board.get(num_called) {
-                let same_row_bingo = board
-                    .values()
-                    .filter(|tile| tile.row == ent.row)
-                    .all(|t| t.called);
-                let same_col_bingo = board
-                    .values()
-                    .filter(|tile| tile.col == ent.col)
-                    .all(|t| t.called);
-                if same_col_bingo || same_row_bingo {
-                    return Some(board);
-                }
-            }
-            winning_board
+            counts
         })
+        .values()
+        .filter(|&count| count.0 >= 2 && count.1)
+        .count()
 }
 
-fn apply_moves(nums_called: Vec<u8>, mut boards: Boards) -> (u32, u32) {
-    let ans: Option<(u32, u32)> = nums_called.iter().fold(None, |cur_ans, num_called| {
-        if let Some(_) = cur_ans {
-            return cur_ans;
+#[derive(PartialEq, Eq)]
+enum LineType {
+    Vert,
+    Horiz,
+    Diag,
+    Unknown,
+}
+
+fn get_line_type(l: &VentLine) -> LineType {
+    let Coord(x1, y1) = l.0;
+    let Coord(x2, y2) = l.1;
+    if x1 == x2 {
+        LineType::Vert
+    } else if y1 == y2 {
+        LineType::Horiz
+    } else if (y2 - y1).abs() == (x2 - x1).abs() {
+        LineType::Diag
+    } else {
+        LineType::Unknown
+    }
+}
+
+fn line_to_coords(std_line: &String) -> Option<VentLine> {
+    std_line
+        .split_whitespace()
+        .take(3)
+        .map(|l| l.to_string())
+        .enumerate()
+        .filter_map(|(idx, chunk)| {
+            if idx != 1 {
+                // -> in the middle of the line
+                let (x, y) = chunk
+                    .split(",")
+                    .map(|c| c.parse::<i32>().unwrap())
+                    .next_tuple::<(i32, i32)>()
+                    .unwrap();
+                Some(Coord(x, y))
+            } else {
+                None
+            }
+        })
+        .next_tuple()
+}
+
+fn get_vent_lines(std_lines: std::io::Lines<StdinLock<'_>>) -> VentLines {
+    std_lines.fold(VentLines::new(), |mut state, line| {
+        if let Ok(ref line) = line {
+            if line == "" {
+                return state;
+            }
+            if let Some(coords) = line_to_coords(&line) {
+                let lt = get_line_type(&coords);
+                match lt {
+                    LineType::Diag | LineType::Vert | LineType::Horiz => state.push(coords),
+                    _ => (),
+                };
+            }
         }
-        let winning_board = apply_move(num_called, &mut boards);
-        if let Some(wb) = winning_board {
-            let unmarked_sum: u32 = wb
-                .iter()
-                .filter(|(_, v)| !v.called)
-                .map(|tup| *tup.0 as u32)
-                .sum();
-            Some((unmarked_sum, *num_called as u32))
-        } else {
-            cur_ans
-        }
-    });
-    ans.unwrap()
+        state
+    })
 }
 
 fn main() -> Result<(), ()> {
     let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    let nums_called: Vec<u8> = lines
-        .next()
-        .unwrap()
-        .unwrap()
-        .split(",")
-        .map(|num_str| {
-            let num: u8 = num_str.parse().unwrap();
-            num
-        })
-        .collect();
-    let boards = get_boards(lines);
-    let (unmarked, winning_num) = apply_moves(nums_called, boards);
-    println!(
-        "ans = unmarked * winning_num = {} * {} = {}",
-        unmarked,
-        winning_num,
-        unmarked * winning_num
-    );
+    let stdin_lines = stdin.lock().lines();
+    let vent_lines = get_vent_lines(stdin_lines);
+    println!("ans = {}", num_intersections(vent_lines));
     Ok(())
 }
