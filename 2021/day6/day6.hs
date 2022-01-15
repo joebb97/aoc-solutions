b25376ba17507c112a73fa067b9fc999e307ec0e
Joseph Buiteweg <joebb@umich.edu>
2022-01-15 12:42:18 -0500

    Finish day6, IN HASKELL!!

---
+++ 2021/day6/day6.hs
@@ -0,0 +1,35 @@
+import Data.List (sort, group)
+import System.Environment (getArgs)
+import Data.Char (digitToInt, isDigit)
+
+main :: IO ()
+main = do
+    contents <- getContents
+    args <- getArgs
+    let numDays = (+1) $ read $ head args :: Int
+    let intList = initialCounts $ toIntList contents
+    let days = last $ take numDays $ iterate applyDay intList
+    let asStr = show $ intList
+    putStrLn $ "res = " ++ (show $ sum days)
+
+daysIndices = [0..8]
+initialCounts :: [Int] -> [Int]
+initialCounts arg = map (\pos -> (length $ only pos arg)) daysIndices
+
+only :: Int -> [Int] -> [Int]
+only x = filter (==x) 
+
+rotateLeft :: [Int] -> [Int]
+rotateLeft list = xs ++ [x]
+    where x:xs = list
+
+applyDay :: [Int] -> [Int]
+applyDay timers = zipWith updateTimers daysIndices rotated
+    where
+    numZeros = head timers
+    rotated = rotateLeft timers
+    updateTimers idx timer = if idx == 6 then timer + numZeros else timer
+
+
+toIntList :: String -> [Int]
+toIntList = map digitToInt . filter isDigit
