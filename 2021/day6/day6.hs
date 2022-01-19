c28974454c2e6ab2f697f88c65122fd992309584
Joseph Buiteweg <joebb@umich.edu>
2022-01-18 23:55:19 -0500

    Apply hindent, looks good

--- 2021/day6/day6.hs
+++ 2021/day6/day6.hs
@@ -1,34 +1,38 @@
-import System.Environment (getArgs)
 import Data.Char (digitToInt, isDigit)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-    contents <- getContents
-    args <- getArgs
-    let numDays = (+1) $ read $ head args :: Int
-    let intList = initialCounts $ toIntList contents
-    let days = last $ take numDays $ iterate applyDay intList
-    let asStr = show $ intList
-    putStrLn $ "res = " ++ (show $ sum days)
+  contents <- getContents
+  args <- getArgs
+  let numDays = (+ 1) $ read $ head args :: Int
+  let intList = initialCounts $ toIntList contents
+  let days = last $ take numDays $ iterate applyDay intList
+  let asStr = show $ intList
+  putStrLn $ "res = " ++ (show $ sum days)
 
-daysIndices = [0..8]
+daysIndices = [0 .. 8]
+
 initialCounts :: [Int] -> [Int]
 initialCounts arg = map (\pos -> (length $ only pos arg)) daysIndices
 
 only :: Int -> [Int] -> [Int]
-only x = filter (==x) 
+only x = filter (== x)
 
 rotateLeft :: [Int] -> [Int]
 rotateLeft list = xs ++ [x]
-    where x:xs = list
+  where
+    x:xs = list
 
 applyDay :: [Int] -> [Int]
 applyDay timers = zipWith updateTimers daysIndices rotated
-    where
+  where
     numZeros = head timers
     rotated = rotateLeft timers
-    updateTimers idx timer = if idx == 6 then timer + numZeros else timer
-
+    updateTimers idx timer =
+      if idx == 6
+        then timer + numZeros
+        else timer
 
 toIntList :: String -> [Int]
 toIntList = map digitToInt . filter isDigit
