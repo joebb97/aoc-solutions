203d01bea7fa84a2f37e31b0a391c91adc808ee5
Joseph Buiteweg <joebb@umich.edu>
2022-01-18 23:54:29 -0500

    Finish day7

--- 2021/day7/day7.hs
+++ 2021/day7/day7.hs
@@ -1,34 +1,55 @@
-import System.Environment (getArgs)
 import Data.Char (digitToInt, isDigit)
+import qualified Data.List as List
+import qualified Data.Ord as Ord
 
 main :: IO ()
 main = do
-    contents <- getContents
-    args <- getArgs
-    let numDays = (+1) $ read $ head args :: Int
-    let intList = initialCounts $ toIntList contents
-    let days = last $ take numDays $ iterate applyDay intList
-    let asStr = show $ intList
-    putStrLn $ "res = " ++ (show $ sum days)
+  contents <- getContents
+  let intList = toIntList contents
+  let means = [f . mean $ l | f <- [floor, ceiling], let l = intList]
+  let res =
+        minimum
+          [sum . map (f m) $ intList | m <- means, let f = distanceFromPos]
+  print ("res=", res)
 
-daysIndices = [0..8]
-initialCounts :: [Int] -> [Int]
-initialCounts arg = map (\pos -> (length $ only pos arg)) daysIndices
+mean :: (Num a, Integral a) => [a] -> Float
+mean list = sum asFracs / fromIntegral (length asFracs)
+  where
+    asFracs = map fromIntegral list :: [Float]
 
-only :: Int -> [Int] -> [Int]
-only x = filter (==x) 
+medianWith :: (Num a, Integral a) => (a -> a) -> [a] -> (a, a)
+medianWith func list = middleElt sorted
+  where
+    sorted = List.sortOn snd elts
+    funcApplied = List.map func list
+    elts = zip list funcApplied
 
-rotateLeft :: [Int] -> [Int]
-rotateLeft list = xs ++ [x]
-    where x:xs = list
+middleElt :: [a] -> a
+middleElt list = list !! (div len 2)
+  where
+    len = length list
 
-applyDay :: [Int] -> [Int]
-applyDay timers = zipWith updateTimers daysIndices rotated
-    where
-    numZeros = head timers
-    rotated = rotateLeft timers
-    updateTimers idx timer = if idx == 6 then timer + numZeros else timer
+-- NOTE: Change for part 2
+distanceFromPos :: (Num a, Integral a) => a -> a -> a
+distanceFromPos pos = distModel . abs . subtract pos
 
+-- distanceFromPos pos = abs . subtract pos
+distModel :: (Num a, Integral a) => a -> a
+distModel num = (num * (num + 1)) `div` 2
 
 toIntList :: String -> [Int]
-toIntList = map digitToInt . filter isDigit
+toIntList =
+  map read .
+  words .
+  map
+    (\c ->
+       if c == ','
+         then ' '
+         else c)
+-- NOTE: I thought the mode was what was needed, turns out it's the median.
+-- The code below does work though.
+-- import qualified Data.Map as Map
+-- mode :: (Ord a, Num a) => [a] -> a
+-- mode = fst . List.maximumBy (Ord.comparing snd) . Map.toList . modalities
+-- modalities :: Ord k => Num a => [k] -> Map.Map k a
+-- modalities = Map.fromListWith (+) . map (\a -> (a, 1))
