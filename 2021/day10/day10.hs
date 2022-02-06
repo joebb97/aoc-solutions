5f2fa4f5fba13738a366c41b79fe8f914a2ac572
Joseph Buiteweg <joebb@umich.edu>
2022-02-05 22:14:56 -0500

    finish day10 pt1

--- 2021/day10/day10.hs
+++ 2021/day10/day10.hs
@@ -1,12 +1,12 @@
 import Control.Monad
 import Data.Char (digitToInt, isDigit)
+import qualified Data.Either as Either
 import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 import qualified Data.Ord as Ord
 import qualified Data.Set as Set
 import qualified Data.Tuple as Tuple
-import qualified Data.Either as Either
 
 main :: IO ()
 main = do
@@ -24,27 +24,23 @@ handleParen curStack c = do
   let isClose = Map.lookup c closeParenScores
   case (isClose, curStack) of
     (Just score, []) -> Left score
-    (Just score, x:xs) -> 
-        let closing = Map.lookup x parenInfos
-        in if closing == Just c then
-            return xs
-        else
-            Left score
-    (Nothing, _) -> return (c:curStack)
+    (Just score, x:xs) ->
+      let closing = Map.lookup x parenInfos
+       in if closing == Just c
+            then return xs
+            else Left score
+    (Nothing, _) -> return (c : curStack)
 
 parenInfos = Map.fromList parensPairs
   where
     parensPairs = [('(', ')'), ('{', '}'), ('<', '>'), ('[', ']')]
 
 closeParenScores = Map.fromList [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
-    
 
 -- Current stack
-type NoMismatch =
-  [Char]
+type NoMismatch = [Char]
 
 -- Number of points
-type Mismatch =
-  Int
+type Mismatch = Int
 
 type InspectState = Either Mismatch NoMismatch
